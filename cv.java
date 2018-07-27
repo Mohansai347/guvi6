4f6210358472380a70367659f4343779f9137efe
Mohansai347 <41725622+Mohansai347@users.noreply.github.com>
2018-07-27 11:12:13 +0530

    Update cv.java

--- cv.java
+++ cv.java
@@ -1,10 +1,6 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -12,7 +8,6 @@ class Ideone
 		int i=0;
 	Scanner sc=new Scanner(System.in);
 	char ch=sc.next().charAt(0);	 
-	
 	switch(ch)
 	{
 	 case  'a' :
@@ -28,9 +23,9 @@ class Ideone
 	}
 	if(i==1)
 	System.out.println("vowel");
-	else 
+       else
 		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-		System.out.println("Consonent");
+		System.out.println("Consonant");
 		else
 		System.out.println("Invalid");		
 	}
