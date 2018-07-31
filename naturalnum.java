388045f8865797e06502a939c61968466c037e77
Mohansai347 <41725622+Mohansai347@users.noreply.github.com>
2018-07-31 11:36:59 +0530

    Create naturalnum.java

---
+++ naturalnum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a=(n*(n+1))/2;
+		System.out.println(a);
+	}
+}
