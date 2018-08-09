06fb40a6cbba6af26a11aad489b708c93ad8c70b
Mohansai347 <41725622+Mohansai347@users.noreply.github.com>
2018-08-09 10:50:07 +0530

    Create eveninterval.java

---
+++ eveninterval.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int num1 = s.nextInt();
+		int num2 = s.nextInt();
+		for(int i=num1+1;i<num2;i++)
+		{
+			if(i%2==0)
+			System.out.println(i+"");		}
+
+	}
+}
