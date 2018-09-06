a6298d085b4757fe9e514479a4cee7eeff7115ca
Mohansai347 <41725622+Mohansai347@users.noreply.github.com>
2018-09-06 12:45:07 +0530

    Update numericdigits.java

--- numericdigits.java
+++ numericdigits.java
@@ -4,14 +4,14 @@ import java.io.*;
 class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
-	{
+	{	
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int count=0;
 		while(n>0)
 		{
-			n=n/10;
-			count++;
+		n=n/10;
+		count++;
 		}
 		System.out.println(count);
 	}
